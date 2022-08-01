2da0fc83541a2c6d54f2b08a0ea67ecf9473aa54
evheath <elliot.v.heath@gmail.com>
2022-08-01 14:41:09 -0500

    center text always

--- src/components/GetItBuilt.tsx
+++ src/components/GetItBuilt.tsx
@@ -5,7 +5,7 @@ export default function GetItBuilt({ }) {
   return (
     <>
       <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-        <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+        <div className="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-6 lg:px-0 lg:text-left lg:flex lg:items-center">
           <div className="lg:py-24">
             <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
               <span className="block">Need an app?</span>
