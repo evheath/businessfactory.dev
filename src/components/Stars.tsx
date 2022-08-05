5fcea8a267411009037454b4bfac626a1afaa829
evheath <elliot.v.heath@gmail.com>
2022-08-05 14:50:08 -0500

    stars is its own component now

---
+++ src/components/Stars.tsx
@@ -0,0 +1,29 @@
+import { CSSProperties, useEffect, useState } from "react";
+
+export default function Stars({ }) {
+  const [styleObj, setStyleObj] = useState({} as CSSProperties);
+  function calculateStars(width: number, height: number) {
+    let shadow = '';
+    const count = Math.floor(Math.sqrt(width * height) / 14);
+    for (let index = 0; index < count; index++) {
+      shadow += `${Math.random() * width}px ${Math.random() * height}px #fff, `;
+    }
+    let stars = shadow.slice(0, shadow.length - 2);
+    setStyleObj({ boxShadow: `${stars}` });
+  }
+
+  // useEffect does not run on server-side rendering, 
+  // so we can get the screen size from the window object here
+  useEffect(() => {
+    calculateStars(window.innerWidth, window.innerHeight);
+    window.addEventListener('resize', () => {
+      calculateStars(window.innerWidth, window.innerHeight);
+    })
+  }, [])
+
+  return (
+    <>
+      <div className='star' style={styleObj}></div>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,32 +5,17 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Container } from "@/components/Container";
 import Head from 'next/head'
-import { CSSProperties } from "react";
+import Stars from "@/components/Stars";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  let shadow = '';
-  const w = typeof window !== "undefined" ? window.screen.width : 1920;
-  const h = typeof window !== "undefined" ? window.screen.height : 1080;
-  console.log(w, h);
-  const count = Math.floor(Math.sqrt(w * h) / 14);
-  for (let index = 0; index < count; index++) {
-    shadow += `${Math.random() * w}px ${Math.random() * h}px #fff, `;
-  }
-  let stars = shadow.slice(0, shadow.length - 2);
-  console.log(stars);
-  const styleObj: CSSProperties = {
-    boxShadow: `${stars}`
-  }
   return (<>
     <Head>
-      {/* the favicon */}
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <div className='star' style={styleObj}>Hello app</div>
+    <Stars />
     <Container >
       <Header />
-      {/* <Breadcrumb /> */}
     </Container>
     <Component {...pageProps} />
     <Footer />
