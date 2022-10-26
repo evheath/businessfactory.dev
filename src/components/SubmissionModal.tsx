0a42df01dc5846b8743f4daf17f07dfd5297ad39
evheath <elliot@businessfactory.dev>
2022-10-26 12:30:21 -0500

    Routing away after submission

--- src/components/SubmissionModal.tsx
+++ src/components/SubmissionModal.tsx
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { useRouter } from "next/router";
 
 export default function ProfileModal({ onClose }: { onClose: () => void }) {
+  const router = useRouter();
   return (
     <Transition.Root show={true} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={onClose}>
@@ -28,14 +30,22 @@ export default function ProfileModal({ onClose }: { onClose: () => void }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-black px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <Dialog.Title className="text-lg font-medium text-white">
+                  Form submitted!
+                </Dialog.Title>
+                <Dialog.Description className="mt-2 text-sm text-gray-300">
+                  I&apos;ll be reaching out to you soon.
+                </Dialog.Description>
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="font-medium inline-flex w-full justify-center rounded-md border border-transparent bg-pink-600 px-4 py-2 text-base text-white shadow-sm hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 sm:text-sm"
-                    onClick={onClose}
+                    className="font-medium inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                    onClick={() => {
+                      router.push("/blog");
+                    }}
                   >
-                    Message
+                    Check out the blog
                   </button>
                 </div>
               </Dialog.Panel>
