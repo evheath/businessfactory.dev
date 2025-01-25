7e6749756d73e1cbada7a817e47347acaa71bb65
evheath <elliot@businessfactory.dev>
2025-01-25 10:20:20 -0600

    portfolio looking better on mobile

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,8 +1,8 @@
 import ProjectCard from "@/components/portfolio-project-card";
 const Page = () => {
   return (
-    <div className="m-24">
-      <h1 className="text-white text-5xl font-medium font-sans">
+    <div className="my-6 mx-1">
+      <h1 className="text-white text-5xl font-medium font-sans text-center">
         My Portfolio
       </h1>
       <div className="my-8" />
